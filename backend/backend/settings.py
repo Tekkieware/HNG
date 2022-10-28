80731661386bbdeb7a676108ff50f115f62cb49b
Tekkieware <tekkieware@gmail.com>
2022-10-28 15:32:42 +0100

    cor fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
 
     #my apps
     'stage1.apps.Stage1Config',
-    'rest_framework'
+    'rest_framework',
+    "corsheaders",
     ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'backend.urls'
